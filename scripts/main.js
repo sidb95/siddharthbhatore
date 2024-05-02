8110604ba02d0c4c47913a09a45367fca83a0671
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-02 15:28:56 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 // this is where the magic happens.
 /* */
-port = 8080
-let imgFiles = ["../static/images/email.png", "../static/images/github.svg", "../static/images/siddh.jpg", "../static/images/friend.png"];
-//
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() { // 
-    if(this.readyState == 4 && this.status == 200){
-        var data = JSON.parse(this.response);
-        autoFill(imgFiles, data); // function autoFill() resides in scriptAuto.js
+def main():
+  port = 8080;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function( {
+    if(this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.response);
+      autoFill(imgFiles, data); 
+// function autoFill() resides //  scriptAuto.js
     }
-}
-xhttp.open('GET', process.env.PORT || port, true);
-xhttp.send();
+  }
+  xhttp.open('GET', process.env.PORT || port, true);
+  xhttp.send();

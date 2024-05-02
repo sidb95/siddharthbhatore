46f6c46f709fdf52f5ff1fc8215f18f8a0d03523
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-02 15:16:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ var fs = require('fs');
 
 var hostname = 'localhost';
 const port = 8080;
-
+let dataOne = {}
 const server = http.createServer(function (req, res) {
     fs.readFile('./static/files/datafile1.json', 'utf-8', function (err, data) {
         if (err) throw err;
-        res.writeHead(200, {'Content-Type' : 'text/html', "access-control-allow-origin": null});
+        res.writeHead(200, {'Content-Type' : 'html', "access-control-allow-origin": null});
+        dataOne = data
         return res.end();
     });
 });
 
 server.listen(process.env.PORT || port, function() {
+    autoFill([], dataOne);
     console.log(`server running at http://${process.env.PORT || port}`);
 });

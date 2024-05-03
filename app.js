95d253b5224ad7cb4643989fbdc604e410d7256e
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-03 12:36:50 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import http
+import fs
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});

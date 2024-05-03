22ab70738cd82a93cfe53ea405a587e56ddd5a79
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-03 12:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
-    const server = http.createServer(function(request, response) {  
+    http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    });
-});
-
-server.listen(process.env.PORT || port, function() {
+    }).listen(process.env.PORT || port, function() {
     console.log(`server running at http://${process.env.PORT || port}`);
 });
